7696fc32a092b18b6e12e8d075cb89eb413eaf04
Chutiendat26092003 <91305197+Chutiendat26092003@users.noreply.github.com>
2022-06-03 21:45:36 +0700

    update css

--- angularjs_weather/js/weather.js
+++ angularjs_weather/js/weather.js
@@ -10,16 +10,22 @@ weatherApp.controller('WeatherCtrl', function ($scope, $http) {
                 $scope.temp_min = data.main.temp_min;
                 $scope.temp_max = data.main.temp_max;
                 $scope.wind_speed = data.wind.speed;
-                $scope.clouds = data.clouds ? data.clouds.all : undefined;
+                var icon =  data.weather[0].icon;
+                $scope.img_url =  'http://openweathermap.org/img/wn/' + icon + '@2x.png';
 
-                var baseUrl = 'https://ssl.gstatic.com/onebox/weather/128/';
-                if ($scope.clouds < 20) {
-                    $scope.img_url = baseUrl + 'sunny.png';
-                } else if ($scope.clouds < 90) {
-                    $scope.img_url = baseUrl + 'partly_cloudy.png';
-                } else  {
-                    $scope.img_url = baseUrl + 'cloudy.png';
-                }
+
+
+
+                // $scope.clouds = data.clouds ? data.clouds.all : undefined;
+                //
+                // var baseUrl = 'https://ssl.gstatic.com/onebox/weather/128/';
+                // if ($scope.clouds < 20) {
+                //     $scope.img_url = baseUrl + 'sunny.png';
+                // } else if ($scope.clouds < 90) {
+                //     $scope.img_url = baseUrl + 'partly_cloudy.png';
+                // } else  {
+                //     $scope.img_url = baseUrl + 'cloudy.png';
+                // }
             }
         })
         .error(function (data, status) {
